ec0b43a7d826bd66357035af6ae69ab70c6dc88c
Sara Sanchez <ssancam391@g.educaand.es>
2024-04-22 22:55:33 +0200

    readme

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -27,17 +27,13 @@ import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.LineHeightStyle.Alignment.*
 
 fun main() = application {
-
-    // Es una interface con una serie de métodos.
     val windowsState = rememberWindowState(size = DpSize(1200.dp, 800.dp))
 
-    // Ésta línea será siempre igual, básico para nuestro programa.
     Window(
         onCloseRequest = ::exitApplication,
         title = "Ejemplo para practicar",
         state = windowsState
     ) {
-        // Llamamos a la función de antes
 
         Ejercicio1()
         // Ejercicio2()
