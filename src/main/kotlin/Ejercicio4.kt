3560f6f3cc85d89e476d7c4d9b6c92a4fc9b63e2
Sara Sanchez <ssancam391@g.educaand.es>
2024-04-24 10:51:51 +0200

    Ejercicio4 terminado

--- src/main/kotlin/Ejercicio4.kt
+++ src/main/kotlin/Ejercicio4.kt
@@ -9,71 +9,75 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment.Vertical
 
 @Composable
 @Preview
 
 fun Ejercicio4() {
     // Cajas en lína horizontal.
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 32.dp),
-        verticalAlignment = Arrangement.Bottom,
-        horizontalArrangement = Arrangement.SpaceEvenly
 
-    ) {
-        // Rectángulo1
-        Box(
+Column(
+    modifier = Modifier
+        .fillMaxSize(),
+    verticalArrangement = Arrangement.Bottom,
+    horizontalAlignment = Alignment.CenterHorizontally
+)
+    {
+        Row(
             modifier = Modifier
-                .height(500.dp)
-                .width(75.dp)
-                .border(BorderStroke(2.dp, Color.Red), shape = RectangleShape)
-                .align(Alignment.Bottom)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.Bottom
         ) {
-            Text(
-                text = "Ejemplo 1",
-                modifier = Modifier.align(Alignment.TopCenter)
-            )
-        }
-        // Rectángulo 2
-        Box(
-            modifier = Modifier
-                .height(400.dp)
-                .width(75.dp)
-                .border(BorderStroke(2.dp, Color.Blue), shape = RectangleShape)
-                .align(Alignment.Bottom)
-        ) {
-            Text(
-                text = "Ejemplo 2",
-                modifier = Modifier.align(Alignment.TopCenter)
-            )
-        }
-        // Rectángulo 3
-        Box(
-            modifier = Modifier
-                .height(300.dp)
-                .width(75.dp)
-                .border(BorderStroke(2.dp, Color.Red), shape = RectangleShape)
-                .align(Alignment.Bottom)
-        ) {
-            Text(
-                text = "Ejemplo 3",
-                modifier = Modifier.align(Alignment.TopCenter)
-            )
-        }
-        // Rentángulo 4
-        Box(
-            modifier = Modifier
-                .height(200.dp)
-                .width(75.dp)
-                .border(BorderStroke(2.dp, Color.Blue), shape = RectangleShape)
-                .align(Alignment.Bottom)
-        ) {
-            Text(
-                text = "Ejemplo 4",
-                modifier = Modifier.align(Alignment.TopCenter)
-            )
+            // Rectángulo1
+            Box(
+                modifier = Modifier
+                    .height(500.dp)
+                    .width(75.dp)
+                    .border(BorderStroke(2.dp, Color.Red), shape = RectangleShape)
+            ) {
+                Text(
+                    text = "Ejemplo 1",
+                    modifier = Modifier.align(Alignment.TopCenter)
+                )
+            }
+            // Rectángulo 2
+            Box(
+                modifier = Modifier
+                    .height(350.dp)
+                    .width(75.dp)
+                    .border(BorderStroke(2.dp, Color.Blue), shape = RectangleShape)
+            ) {
+                Text(
+                    text = "Ejemplo 2",
+                    modifier = Modifier.align(Alignment.TopCenter)
+                )
+            }
+            // Rectángulo 3
+            Box(
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(75.dp)
+                    .border(BorderStroke(2.dp, Color.Red), shape = RectangleShape)
+            ) {
+                Text(
+                    text = "Ejemplo 3",
+                    modifier = Modifier.align(Alignment.TopCenter)
+                )
+            }
+            // Rentángulo 4
+            Box(
+                modifier = Modifier
+                    .height(50.dp)
+                    .width(75.dp)
+                    .border(BorderStroke(2.dp, Color.Blue), shape = RectangleShape)
+            ) {
+                Text(
+                    text = "Ejemplo 4",
+                    modifier = Modifier.align(Alignment.TopCenter)
+                )
+            }
         }
     }
 }
